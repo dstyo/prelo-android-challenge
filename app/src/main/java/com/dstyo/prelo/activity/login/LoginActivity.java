2f99a7b010b58cd04cf1a89efae1b16d3b8fcaf9
Dwi Setiyono <dstyo91@gmail.com>
2017-12-07 00:42:10 +0700

    Prepare Login Layout

---
+++ app/src/main/java/com/dstyo/prelo/activity/login/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.dstyo.prelo.activity.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dstyo.prelo.R;
+
+/**
+ * @author Dwi Setiyono <dwi.setiyono@ovo.id>
+ * @since 2017.06.12
+ */
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/dstyo/prelo/activity/main/MainActivity.java
+++ app/src/main/java/com/dstyo/prelo/activity/main/MainActivity.java
@@ -1,4 +1,4 @@
-package com.dstyo.prelo;
+package com.dstyo.prelo.activity.main;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -8,6 +8,8 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+
+import com.dstyo.prelo.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
